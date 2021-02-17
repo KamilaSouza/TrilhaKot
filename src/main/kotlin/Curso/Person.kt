30cd8495526204b4127ae2104582d0581c4feeae
KamilaSouza <kamisouzas@gmail.com>
2021-02-17 18:15:36 -0300

    Adicionando classe pessoa

---
+++ src/main/kotlin/Curso/Person.kt
@@ -0,0 +1,21 @@
+package Curso
+
+class person {
+    var name: String = "Kamila"
+    var cpf: String = "123.456.789-00"
+        private set
+
+
+    fun infopessoa () = "$name e $cpf"
+
+    inner class Endereco {
+        var rua: String = "Rua Teste"
+    }
+}
+
+fun main (){
+    val kamila = person()
+    println(kamila.infopessoa())
+    println(kamila.Endereco().rua)
+}
+
